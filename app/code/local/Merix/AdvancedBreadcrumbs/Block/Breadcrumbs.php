3fe4afd300312e992822d17fc62e321976b288a7
Daniel Kucal <d.kucal@merixstudio.com>
2015-08-18 10:20:28 +0200

    queries optimization

--- app/code/local/Merix/AdvancedBreadcrumbs/Block/Breadcrumbs.php
+++ app/code/local/Merix/AdvancedBreadcrumbs/Block/Breadcrumbs.php
@@ -3,6 +3,8 @@
 class Merix_AdvancedBreadcrumbs_Block_Breadcrumbs extends Mage_Page_Block_Html_Breadcrumbs
 {
 
+    protected $_allCategories = array();
+
     public function __construct()
     {
         parent::__construct();
@@ -14,32 +16,42 @@ class Merix_AdvancedBreadcrumbs_Block_Breadcrumbs extends Mage_Page_Block_Html_B
 
     protected function _beforeToHtml()
     {
-        if (!$this->_crumbs) {
+        if (!$this->_crumbs || !(Mage::registry('current_category') || Mage::registry('current_product'))) {
             return parent::_beforeToHtml();
         }
 
         $crumbs = array_values($this->_crumbs);
         $keys = array_keys($this->_crumbs);
 
-        foreach($crumbs as $i => $crumb)
-        {
-            $category = Mage::getResourceModel('catalog/category_collection')
-                ->addFieldToFilter('name', $crumb['label'])
-                ->getFirstItem();
-
-            $this->_crumbs[$keys[$i]]['subcategories'] = array();
-
-            if($i > 0){
-                $category
-                    ->getChildrenCategories()
-                    ->addFieldToFilter('name', $crumbs[$i-1]['label'])
-                    ->getFirstItem();
+        $parentIds = array();
+        foreach ($keys as $i => $key) {
+            if (strpos($key, 'category') !== false) {
+                $catId = str_replace('category', '', $key);
+                $crumbs[$i]['category_id'] = $catId;
+                $parentIds[] = $catId;
             }
-            if($category->getData()){
-                $children = $category->getChildrenCategories();
+        }
 
-                foreach($children as $child)
-                {
+        $collection = Mage::getModel('catalog/category')->getCollection()
+            ->addAttributeToSelect('id')
+            ->addAttributeToSelect('name')
+            ->addAttributeToSelect('url_key')
+            ->addAttributeToSelect('url')
+            ->addAttributeToSelect('is_active')
+            ->addAttributeToFilter('parent_id', array('in' => $parentIds))
+            ->addAttributeToSort('position');
+        $categoryToParent = array();
+        foreach ($collection as $category) {
+            $categoryToParent[$category->getParentId()][] = $category;
+        }
+
+        foreach ($crumbs as $i => $crumb) {
+            if (!isset($crumb['category_id'])) {
+                continue;
+            }
+            if (!empty($categoryToParent[$crumb['category_id']])) {
+                $categoryId = $crumb['category_id'];
+                foreach ($categoryToParent[$categoryId] as $child) {
                     $this->_crumbs[$keys[$i]]['subcategories']['category' . $child->getId()] = array(
                         'label' => $child->getName(),
                         'link' => $child->getUrl(),
@@ -59,7 +71,7 @@ class Merix_AdvancedBreadcrumbs_Block_Breadcrumbs extends Mage_Page_Block_Html_B
         $parser = parse_url($url);
         $path = isset($parser['path']) ? $parser['path'] : '';
         $path = explode('/', $path);
-        $path[count($path)-1] = basename($path[count($path)-1], '.html');
+        $path[count($path) - 1] = basename($path[count($path) - 1], '.html');
         return array_filter($path);
     }
 
